83f5a067f274982d22ac2396a89b42b36600b94e
yLeonardo99 <leo205283@gmail.com>
2024-09-17 18:54:16 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Site_ong
+<!-- Fiz um site para uma ong com o proposito de trazer mais visibilidade e arrecadações 
+![image](https://user-images.githubusercontent.com/88861947/218461648-82e38874-4be1-466d-a356-e549dadb9626.png)
+![image](https://user-images.githubusercontent.com/88861947/218462992-7f6684ef-3a34-4ae9-9c44-64db449b4207.png) -->
+
+
+
---
+++ js/script.js
@@ -0,0 +1,34 @@
+let myCarousel = document.getElementById('myCarousel');
+let carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 2000,
+  wrap: false
+});
+
+myCarousel.addEventListener('slide.bs.carousel', function () {
+  //  executar quando o evento 'slide' ocorrer
+
+});
+
+
+// DESENVOLVEDORES
+
+document.addEventListener('mousemove', (event) => {
+  const pet = document.getElementById('adopt-pet');
+  if (pet) {
+    const rect = pet.getBoundingClientRect();
+    const x = event.clientX - rect.left - (rect.width / 2);
+    const y = event.clientY - rect.top - (rect.height / 2);
+
+    pet.style.transform = `translate(${x / 20}px, ${y / 20}px)`;
+  }
+});
+
+//lOGO 
+
+document.querySelector('.logo').addEventListener('click', () => {
+  alert('Logo clicada! Bem-vindo à ONG Pet Friends!');
+});
+
+
+
+
